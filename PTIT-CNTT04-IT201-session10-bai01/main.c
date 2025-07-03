84be690359de848ccc3e95ac1fdfc24f761a896c
ChloeAmelinda <trangthichngunuong@gmail.com>
2025-07-03 15:59:54 +0700

    Add files via upload

---
+++ PTIT-CNTT04-IT201-session10-bai01/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//cau truc
+struct Node {
+    int data;
+    struct Node *next;
+};
+struct Node* createNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    if (temp == NULL    ) {
+        printf("Memory allocation error");
+        exit(-1);
+    }
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+struct Node* createLinkedList() {
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+    printf("Enter the elements of the linked list:\n");
+    for (int i = 1; i <= 5; i++) {
+        int data;
+        printf("node %d:",i);
+        scanf("%d",&data);
+        struct Node* node = createNode(data);
+        if (head == NULL) {
+            head = node;
+            temp = node;
+        }else{
+           temp->next = node;
+            temp = node;
+        }
+
+    }
+    return head;
+}
+
+int main(void) {
+    struct Node* head = createLinkedList();
+    return 0;
+}
